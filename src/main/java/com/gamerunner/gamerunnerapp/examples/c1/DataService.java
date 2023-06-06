e1cbae3434dab1238ef375f6ff73674b80bfcf0b
ashharr <shaikh.ashhar6@gmail.com>
2023-06-06 17:45:29 +0530

    todo add final

--- src/main/java/com/gamerunner/gamerunnerapp/examples/c1/DataService.java
+++
@@ -1,5 +0,0 @@
-package com.gamerunner.gamerunnerapp.examples.c1;
-
-public interface DataService {
-	int[] retrieveData();
-}
--- src/main/java/com/gamerunner/gamerunnerapp/game/GamingConsole.java
+++
@@ -1,8 +0,0 @@
-package com.gamerunner.gamerunnerapp.game;
-
-public interface GamingConsole {
-	void up();
-	void down();
-	void left();
-	void right();
-}
